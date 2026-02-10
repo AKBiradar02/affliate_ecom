a623115ddf13cb3ed7047ab36be43cfbc999daf1
AKBiradar02 <1032211780@mitwpu.edu.in>
2026-02-11 00:07:30 +0530

    fix: Remove unused Firebase imports

--- src/firebase.js
+++ src/firebase.js
@@ -14,6 +14,6 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-//export const db = getFirestore(app);
+//const analytics = getAnalytics(app);
+export const db = getFirestore(app);
 export const auth = getAuth(app);
